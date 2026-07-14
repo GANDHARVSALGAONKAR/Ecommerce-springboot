6a4db5e6eec1c537524b8809bf12546f2b836045
GANDAHRVSALGAONKAR <gandharvsalgaonkar87@gmail.com>
2026-07-14 09:11:58 +0530

    14 July 2026

--- src/main/java/com/study/ecommerce/security/SecurityConfig.java
+++ src/main/java/com/study/ecommerce/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.study.ecommerce.security;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +16,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.study.ecommerce.security.jwt.AuthTokenFilter;
 
@@ -32,6 +37,7 @@ public class SecurityConfig {
 	{
 		httpSecurity
 		.csrf(csrf->csrf.disable())
+		.cors(Customizer.withDefaults())
 		.authorizeHttpRequests(request->
 		request
 		.requestMatchers(HttpMethod.GET,"/products/**").permitAll()
@@ -68,4 +74,21 @@ public class SecurityConfig {
 	{
 		return authenticationConfiguration.getAuthenticationManager();
 	}
+	
+	
+// GLOBAL CORS
+	@Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = 
+        		new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+        
+    }
 }
